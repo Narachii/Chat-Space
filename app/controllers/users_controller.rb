bdd52e77e6e3d9e54923ba1cb6e2e12e8c064b47
Narachii <naoya706oxford@gmail.com>
2017-07-04 20:31:27 +0900

    Create Sign_in and Log_out function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,13 @@ class UsersController < ApplicationController
     end
   end
 
- private
+  def new
+  end
 
+  def delete
+  end
+
+ private
    def user_params
     params.require(:user).permit(:name, :email)
    end
