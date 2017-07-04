69b5933b34f215163ad166dd1134e8badc12e3b0
Narachii <naoya706oxford@gmail.com>
2017-07-04 19:25:56 +0900

    Modified edit function ver3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,17 +7,13 @@ class UsersController < ApplicationController
      if current_user.update(user_params)
         redirect_to :root
     else
-      render "edit.html.haml"
+      render :edit
     end
   end
 
  private
 
    def user_params
-   params.require(:user).permit(:email, :name)
+    params.require(:user).permit(:email, :name)
    end
-
-  def move_to_index
-  redirect_to controller: :messages, action: 'index'
-  end
 end
