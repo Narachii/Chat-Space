2564c24dc1e306b9b96993fa231e479ebf628a25
Narachii <naoya706oxford@gmail.com>
2017-07-04 21:17:07 +0900

    Edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root "messages#index"
-  resources :users , only:[:edit,:update,:delete,:new]
+  resources :users , except:[:create,:show]
 end
