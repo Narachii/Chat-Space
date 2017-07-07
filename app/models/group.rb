ce480eecac6685f7d9df94750af31e14f2e8002f
Narachii <naoya706oxford@gmail.com>
2017-07-07 19:38:18 +0900

    Edit group.model

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,18 @@ class Group < ApplicationRecord
   has_many :members
   has_many :messages
   has_many :users,through: :members
+
+  def show_last_message(group)
+    if group.messages.present?
+      message = group.messages.order('created_at DESC').find_by('id')
+      if message.body.present?
+        message.body
+      elsif message.image.present?
+        "画像が投稿されました。"
+      end
+    else
+      "新着メッセージはありません。"
+    end
+  end
+
 end
