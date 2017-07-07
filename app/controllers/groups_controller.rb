b2888c35d1ec15fbe0ce3727cd31c70aa81159a1
Narachii <naoya706oxford@gmail.com>
2017-07-07 15:11:19 +0900

    Edit goups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,7 +11,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to root_path,notice: 'グループを作成しました。'
+      redirect_to group_messages_path,notice: 'グループを作成しました。'
     else
       render :new
     end
@@ -22,7 +22,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to groups_path, notice:'グループを編集しました。'
+      redirect_to group_messages_path(group_id: params[:id]), notice:'グループを編集しました。'
     else
       render :edit
     end
