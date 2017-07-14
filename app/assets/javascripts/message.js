791df08ca1fc9e122e83f16eebce8e4d583397d9
Narachii <naoya706oxford@gmail.com>
2017-07-14 18:43:50 +0900

    Modified message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -5,14 +5,15 @@ $(document).on('turbolinks:load',function(){
                 <li class=data>${message.data}</li>
                 <li class=chat>${message.body}</li>
                 </div>   `
-    if (message.image.url != null){
-      var image = `
-              <img src= ${message.image.url}>`
+
+    if (message.image != null){
+    var image = `
+              <img src= ${message.image}>`
+
     var html = html + image
+  };
     return html;
-    };
-    return html;
-  }
+};
 
   $('#new_message').on('submit',function(e){
     e.preventDefault();
@@ -36,4 +37,31 @@ $(document).on('turbolinks:load',function(){
       alert('error');
     });
   });
+
+$(function(){
+  setInterval(function(){
+     if($('.chank')[0]){
+    var message_id = $('.chank:last').attr('data-message-id');
+    // console.log(message_id);
+  } else {
+      var message_id = 0
+  }
+    $.ajax({
+      url: location.href,
+      type: 'GET',
+      data:{
+        message:{id: message_id}
+      },
+      dataType: 'json'
+    })
+    .always(function(data){
+      // console.log(data)
+      $.each(data, function(i,data){
+        var html = buildHTML(data);
+        $('.chats').append(html);
+        $('.chat__middle').animate({scrollTop: $('.chat__middle')[0].scrollHeight});
+      });
+    });
+  },5000);
+});
 });
