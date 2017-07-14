3f7c11cc63daf5c6d945350d3d33ae1ae31900f7
Narachii <naoya706oxford@gmail.com>
2017-07-14 17:24:19 +0900

    Edit messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,8 +2,12 @@ class MessagesController < ApplicationController
 
   def index
     @group = Group.find(params[:group_id])
-    @messages = @group.messages.order('created_at ASC')
     @message = Message.new
+    @messages = @group.messages.order('created_at ASC')
+    respond_to do |format|
+      format.html
+      format.json { @new_message = Message.where('id > ?', params[:message][:id]) }
+    end
   end
 
   def create
@@ -21,6 +25,6 @@ class MessagesController < ApplicationController
 
   private
   def create_params
-    params.require(:message).permit(:body,:image).merge(user_id: current_user.id,group_id: params[:group_id])
+    params.require(:message).permit(:body,:image,:id).merge(user_id: current_user.id,group_id: params[:group_id])
   end
 end
